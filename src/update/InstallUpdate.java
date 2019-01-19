0bab8d89020ebac2c404c4387f7c7e6eb1cb0369
Shailesh Jain <jainshailesh91@gmail.com>
2019-01-19 14:51:16 +0530

    Adding Updater Code

---
+++ src/update/InstallUpdate.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package update;
+
+import java.io.File;
+import java.net.URL;
+import static update.UpdateSoftware.isUpdateAvailable;
+
+/**
+ *
+ * @author Shailesh
+ */
+public class InstallUpdate {
+    public static void downloadAndInstallUpdate(){
+        if(isUpdateAvailable){
+//            File file = new URL(SoftwareDetails.DIST_DOWNLOAD_URL);
+        }
+    }
+}
